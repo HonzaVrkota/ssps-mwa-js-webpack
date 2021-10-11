db00d0d532ac20716ba60da81fef15ea50a3052e
HonzaVrkota <vrkota.jan@hotmail.com>
2021-10-11 11:18:42 +0200

    Add full configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+import { style } from "./main.css";
 import lodash from "lodash";
+import component from "./component.js";
+
 console.log("Hello webpack!");
 
 const fancyFunc = () => {
@@ -10,3 +13,8 @@ const [a, b] = fancyFunc();
 const randomString = "smichovska stredni prumyslova skola";
 
 console.log(lodash.upperCase(randomString));
+
+console.log(lodash.camelCase(randomString));
+
+console.log(a, b);
+document.body.append(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,53 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  mode: "development", //production
+  entry: {
+    main: path.resolve(__dirname, "src/app.js"),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "[name][ext]",
+    clean: true,
   },
+  // devtool: "inline-source-map",
+  // devServer: {
+  //   static: {
+  //     directory: path.resolve(__dirname, "public"),
+  //   },
+  //   port: 5001, //default 8080
+  //   // open: true,
+  //   // hot: true,
+  //   // watchContentBase: true,
+  // },
+  //loaders
+  module: {
+    rules: [
+      //css
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      //images
+      { test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, type: "asset/resource" },
+      //js for babel
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  //plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Just a Demo",
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/temp.html"),
+    }),
+  ],
 };
